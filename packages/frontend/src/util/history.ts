8a3878cdeaccfa1d2e7aab4752f261cc39930301
Dominik Horn <dominik.horn@tum.de>
2020-02-23 12:59:01 +0100

    fix eslint warning

--- packages/frontend/src/util/history.ts
+++ packages/frontend/src/util/history.ts
@@ -3,7 +3,7 @@ import { createBrowserHistory } from 'history';
 /*
  * This is the history object used by react router
  * set configuration options as appropriate
- * 
+ *
  * Having a custom history object permits easier/cleaner
  * manipulation, i.e., programatic navigation between pages
  */
