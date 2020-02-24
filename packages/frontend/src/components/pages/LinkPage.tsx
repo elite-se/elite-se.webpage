a34891d736cd35d94a665fc6b95e0436476973f0
Henrik Wachowitz <ricffb@gmail.com>
2020-02-24 18:58:30 +0100

    skeleton autocomplete

--- packages/frontend/src/components/pages/LinkPage.tsx
+++ packages/frontend/src/components/pages/LinkPage.tsx
@@ -1,4 +1,5 @@
-import { Divider, List } from '@material-ui/core';
+import { Divider, List, TextField } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { LinkListItem } from '../general/LinkListItem';
@@ -9,6 +10,15 @@ export interface LinkPageProps extends RouteComponentProps {}
 export const LinkPage = (props: LinkPageProps) => (
   <>
     <h1>Useful Links List</h1>
+
+    <Autocomplete
+      autoHighlight
+      options={["Meme", "Test"]}
+      renderInput={params => (
+        <TextField {...params} label="autoHighlight" margin="normal" fullWidth />
+      )}
+    />
+
     <List>
       <LinkListItem href={'https://elite-se.informatik.uni-augsburg.de'} title={'Main Webpage'} />
       <LinkListItem href={'https://github.com/elite-se/elite-se.protokolle'} title={'Exam Protocols'} />
