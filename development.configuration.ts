6c8c77de37c7c6f4237b1379514019b6b7487cec
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-02-25 11:21:04 +0100

    add logic for loading configuration files and add example production and development configuration using symlinks

---
+++ development.configuration.ts
@@ -0,0 +1,9 @@
+import { Configuration } from 'elite-types';
+
+const configuration: Configuration = {
+  featureMap: {
+    'under-construction-message': true,
+  },
+};
+
+export default configuration;
