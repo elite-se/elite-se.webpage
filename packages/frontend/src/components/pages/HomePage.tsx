45706a81b258053bd3167a2f9f1ab30934f77769
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-02-25 11:52:27 +0100

    use feature flag component for under construction text

--- packages/frontend/src/components/pages/HomePage.tsx
+++ packages/frontend/src/components/pages/HomePage.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { LinkDirectory } from './support/LinkDirectory';
 import { Divider } from '@material-ui/core';
+import { FeatureFlag } from 'elite-feature-flags';
 
 export interface HomePageProps extends RouteComponentProps {}
 
 export const HomePage = (props: HomePageProps) => (
   <>
     <h1>Main Page</h1>
-    Elite Sexyz is currently under construction. See discord main channel for more information
+    <FeatureFlag featureName="under-construction-message">
+      Elite Sexyz is currently under construction. See discord main channel for more information
+    </FeatureFlag>
     <Divider />
     <LinkDirectory />
   </>
