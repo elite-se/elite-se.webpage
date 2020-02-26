a7126bf58b1b8d856ab6568e5475f3ad507af0d3
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-02-26 12:45:55 +0100

    move configuration files to corresponding package

--- production.configuration.ts
+++
@@ -1,9 +0,0 @@
-import { Configuration } from 'elite-types';
-
-const configuration: Configuration = {
-  featureMap: {
-    'under-construction-message': false,
-  },
-};
-
-export default configuration;
