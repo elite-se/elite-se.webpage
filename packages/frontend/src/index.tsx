7700bef2d82b6cde0d51159e7e36f7a991c41f29
Dominik Horn <dominik.horn@tum.de>
2020-02-27 17:11:08 +0100

    remove precondition test code

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,19 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App';
-import { Pre } from 'elite-decorators';
-
-class Test {
-  private prop: number = 10;
-
-  @Pre(param1 => false)
-  test(param1: number) {
-    console.log('Hi, I received: ', param1);
-    console.log('My Prop is: ', this.prop);
-  }
-}
-
-const test = new Test();
-test.test(10);
 
 ReactDOM.render(<App />, document.getElementById('root'));
