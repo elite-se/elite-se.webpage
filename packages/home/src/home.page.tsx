6fe5b589a3e99b885f60f94a34156b18051d459f
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-02-28 00:21:18 +0100

    extract home page to new package #20

--- packages/home/src/home.page.tsx
+++ packages/home/src/home.page.tsx
@@ -1,10 +1,18 @@
-import { Divider } from '@material-ui/core';
-import { FeatureFlag } from 'elite-feature-flags';
+import { FeatureFlag } from 'elite-components';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { LinkDirectory } from './support/LinkDirectory';
+import { AppPath, AppRoute } from 'elite-types';
 
 export interface HomePageProps extends RouteComponentProps {}
+
+/**
+ * Route for the Home page of this app
+ */
+export const HOME_ROUTE: AppRoute = {
+  path: AppPath.HOME,
+  displayName: 'Home',
+  render: props => <HomePage {...props} />,
+};
 
 export const HomePage = (props: HomePageProps) => (
   <>
@@ -12,7 +20,5 @@ export const HomePage = (props: HomePageProps) => (
     <FeatureFlag featureName="under-construction-message">
       Elite Sexyz is currently under construction. See discord main channel for more information
     </FeatureFlag>
-    <Divider />
-    <LinkDirectory />
   </>
 );
