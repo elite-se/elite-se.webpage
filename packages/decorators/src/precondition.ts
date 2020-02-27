5a06dad33bede97f0971e3417e0c6f8876f51e99
Dominik Horn <dominik.horn@tum.de>
2020-02-27 17:16:16 +0100

    fix linter warnings

--- packages/decorators/src/precondition.ts
+++ packages/decorators/src/precondition.ts
@@ -1,15 +1,13 @@
 export function Pre(condition: (...args: any[]) => boolean) {
-  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor): void => {
     const originalMethod = descriptor.value;
-    descriptor.value = function(...args: any[]) {
-      const context = this;
-
+    descriptor.value = function(...args: any[]): any {
       if (!condition(args)) {
         console.trace(`Precondition violiation in ${target.constructor.name}.${propertyKey}(${args}):`);
         throw new Error(`Precondition doesn't hold for ${target.constructor.name}.${propertyKey}(${args})`);
       }
 
-      return originalMethod.apply(context, args);
+      return originalMethod.apply(this, args);
     };
   };
 }
