82fc410ef086b01013b39a0da864cb110dcea49c
Dominik Horn <dominik.horn@tum.de>
2020-03-02 16:42:53 +0100

    add more links to /links page

--- packages/link/src/link.page.tsx
+++ packages/link/src/link.page.tsx
@@ -1,5 +1,5 @@
 import { List } from '@material-ui/core';
-import { LinkListItem } from 'elite-components';
+import { LinkListItem, LinkListItemProps } from 'elite-components';
 import { AppPath, AppRoute } from 'elite-types';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
@@ -17,9 +17,60 @@ export const LINK_ROUTE: AppRoute = {
   render: props => <LinkPage {...props} />,
 };
 
+// TODO: this is temporary as Henrik will provide a proper linkpage implementation
+// TODO: Idea for @Henrik: categorize links visually, i.e., like tum.sexy
+const links: LinkListItemProps[] = [
+  {
+    href: 'https://elite-se.informatik.uni-augsburg.de',
+    title: 'Study Programm Homepage',
+  },
+  {
+    href: 'https://se-study-planner.herokuapp.com',
+    title: 'SE Study Planner',
+  },
+  {
+    href: 'https://github.com/elite-se/elite-se.protokolle',
+    title: 'Exam Protocols',
+  },
+  {
+    href: 'https://www.elitenetzwerk.bayern.de/elitenetzwerk-home/elitenetzwerk-home/',
+    title: 'Elitenetzwerk Bayern Homepage',
+  },
+  {
+    href: 'https://campus.tum.de',
+    title: 'TUM Offline',
+  },
+  {
+    href: 'https://tools.db.in.tum.de',
+    title: 'TUM DB Systems Tools',
+  },
+  {
+    href: 'https://digicampus.uni-augsburg.de/dispatch.php/my_courses',
+    title: 'UNA Digicampus',
+  },
+  {
+    href: 'https://vibs.uni-augsburg.de/qisserver/pages/cs/sys/portal/hisinoneStartPage.faces?chco=y',
+    title: 'UNA VIBS Portal',
+  },
+  {
+    href: 'https://webmail.rz.uni-augsburg.de/?_task=mail&_mbox=INBOX',
+    title: 'UNA Webmail',
+  },
+  {
+    href: 'https://studiswebstud.zv.uni-augsburg.de/',
+    title: 'UNA STUDIS Portal',
+  },
+  {
+    href: 'https://www.uni-muenchen.de/lmu-intern/index.html',
+    title: 'LMU Webapps Overview',
+  },
+];
+//.sort((a, b) => (a.title < b.title ? -1 : b.title < a.title ? 1 : 0));
+
 export const LinkPage = (props: LinkPageProps) => (
   <List>
-    <LinkListItem href={'https://elite-se.informatik.uni-augsburg.de'} title={'Main Webpage'} />
-    <LinkListItem href={'https://github.com/elite-se/elite-se.protokolle'} title={'Exam Protocols'} />
+    {links.map(link => (
+      <LinkListItem {...link} />
+    ))}
   </List>
 );
